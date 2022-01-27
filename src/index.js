33c6d9f3dbef74a18599b9bba8c62773f4bb959b
Manoj Fernando <manojf@99x.lk>
2022-01-28 02:02:03 +0530

    Updated application code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req,res) => {
-    res.send("Hello from Enlear Academy!");
+    console.log("Serving a new request")
+    res.send("Hello World!");
 });
 
 app.get("/health", (req,res) => {
